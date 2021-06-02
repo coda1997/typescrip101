cf62b8abc8f0e5c1a6ecba23137f4f5c67b537d6
coda1997 <overlooker1997@gmail.com>
2021-06-02 22:31:34 +0800

    update

--- basictypes.ts
+++ basictypes.ts
@@ -1,5 +1,27 @@
 const names = ["Alice", "Bob", "Eve"];
 
-names.forEach(function(s){
+names.forEach(function (s) {
     console.log(s.toLocaleUpperCase())
 });
+
+
+function callee(pt: { x: number, y: number }) {
+    console.log(pt.x);
+    console.log(pt.y);
+}
+//using comma to assgin properties
+callee({ x: 1, y: 2 });
+
+//Optional properties
+function callop(pt: {x: number, y?:string}){
+    console.log(pt.x);
+    // console.log(pt.y);
+    if(pt.y!==undefined){
+        console.log(pt.y);
+    }
+    console.log(pt.y?.toLocaleLowerCase());
+    
+}
+
+callop({x:1,y:"hello"})
+callop({x:1})
---
+++ literaltypes.ts
@@ -0,0 +1,10 @@
+function callee(x:number, align: "left"|"right"|"ceter")
+
+callee(1,"left")
+//throw an error, cause botton are not the type of align
+// callee(2,"botton")
+
+const req = {url:"www.xxx.com", method:"Get"}
+
+req.method = "happy"
+
